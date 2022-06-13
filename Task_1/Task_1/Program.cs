de0b9cd9080e644483802cc10a233daec453b529
SAHV SAHV <metalocalypse_666@mail.ru>
2022-06-13 23:32:54 +0700

    Добавьте файлы проекта.

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestHotels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] citiesArray = { "Москва", "Владивосток", "Красноярск", "Новосибирск", "Санкт-Петербург", "Сочи" };
+            string citiesString = String.Join(", ", citiesArray);
+            citiesString = citiesString + '.';
+            Console.WriteLine(citiesString);
+        }
+    }
+}
